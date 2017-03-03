09b0acaff47093d700ceafc7bd3a47e61cae9198
monad98 <monad98@gmail.com>
2017-03-03 15:13:24 -0800

    refactoring

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 $(document).ready(() => {
+  
   const queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-  const authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+  const authKey = "d8966fd97abd46028557596c31b5fd0e";
+  
   const articleBox = $("#articles");
   const searchTerm = $("#search");
   const numberOfRecords = $("#number");
   const startYear = $("#start-year");
   const endYear = $("#end-year");
+  const searchLabel = $("#search-label");
+  const notFountMsg = $("#not-found");
+  const requiredSearchTermMsg = $("#required-search-term");
+  const searchFormGroup = $("#search-group");
+  const searchTermAppend = $("#search-term-append");
 
-  const convertNumToPages = (num) => {
-    return Math.ceil(num / 10); // number of pages, which means number of ajax call
-  };
-
-  const ajax = (queryObj, page) => {
+  /**
+   * Ajax
+   */
+  // function for ajax request with query
+  const ajax = (queryObj) => {
     return $.ajax({
       type: "GET",
       url: queryURLBase,
@@ -19,21 +26,8 @@ $(document).ready(() => {
     });
   }
 
-  const makeAjaxArray = (howmany, query) => {
-    let result = [];
-    console.log(howmany);
-    for (let i = 0; i < howmany; i++) {
-      const queryObj = Object.assign({}, query, {
-        page: i
-      });
-      result = [...result, ajax(queryObj, i)];
-    }
-    console.log(result);
-    return result;
-  }
-
-  const fetchArticles = (q, startDate, endDate, num = 10) => {
-    const numberOfAjaxCall = convertNumToPages(num);
+  // function for articles request
+  const fetchArticles = (q, startDate, endDate, num) => {
     const query = {
       "api-key": authKey,
       q: q,
@@ -41,30 +35,87 @@ $(document).ready(() => {
       end_date: endDate
     };
 
-    $.when.apply($, makeAjaxArray(numberOfAjaxCall, query))
-      .then(response => {
-        console.log(response);
-        var data = response.response.docs;
-        data.forEach((article, idx) => {
-          if (idx < num) {
-            var temp = $("<div>")
-              .append($("<h3>").text(idx+1 + ". " + article.headline.main)) //title
-              .append($("<p>").text(article.byline.original)) //by
-              .append($("<p>").text("section: " + article.section_name)) // section
-              .append($("<p>").text(new Date(article.pub_date).toString().split(" ").slice(1, 4).join("/"))) //date
-              .append($("<a target=\"_blank\">").attr("href", article.web_url).text(article.web_url)); //url link
-            articleBox.append(temp);
+    // inner function to fetch 10 articles
+    const fetchTenArticles = (query, page) => {
+      let articleCounter = page * 10; //initial article counter for each fetchTenArticles call
+      const queryObj = Object.assign({}, query, { page });
+      ajax(queryObj).done(data => {
+        const articles = data.response.docs; // articles array
+
+        if(!articles.length) {
+          notFountMsg.show("slow");
+          return; //no more ajax call even if number of requested articles is bigger than 10.
+        }
+
+        articles.forEach((article) => {
+          if (articleCounter < num) {
+            renderArticles(article, ++articleCounter);
           }
         });
-      });
-  }; 
 
+        // repeat fetchTenArticles until articleCounter is less than "Number of Records to Retrieve"
+        // request rate limit is 1 per sec. so we use setTimeout function for 1 sec delay.
+        if (articleCounter < num && articles.length === 10) {
+          setTimeout(fetchTenArticles.bind(null, query, ++page), 1000);
+        }
+
+      });
+    };
+
+    //call fetch 10 articles function
+    fetchTenArticles(query, 0); //start from page 0;
+  };
+
+
+  
+  /**
+   * functions for rendering view
+   */
+  function showRequiredMsg() {
+    requiredSearchTermMsg.show("slow");
+    searchFormGroup.addClass("has-error");
+  }
+
+  function clearView() {
+    $("#articles").empty();
+    searchTerm.val(""); //clear input field
+    searchTermAppend.text(""); //clear title;
+    notFountMsg.hide();
+    requiredSearchTermMsg.hide();
+    searchFormGroup.removeClass("has-error");
+  }
+
+  // function for rendering articles
+  function renderArticles(article, articleNumber) {
+    const temp = $("<div>")
+      .append($("<h3>").text(" " + article.headline.main).prepend($("<i class=\"label label-info\">").text(articleNumber))) //title
+      .append($("<p>").text(article.byline ? article.byline.original : "")) //by
+      .append($("<p>").text("section: " + article.section_name)) // section
+      .append($("<p>").text(new Date(article.pub_date).toString().split(" ").slice(1, 4).join("/"))) //date
+      .append($("<a target=\"_blank\">").attr("href", article.web_url).text(article.web_url)); //url link
+    articleBox.append(temp);
+  }
+
+
+  /**
+   * Event Listen
+   */
   $("#search-btn").on("click", () => {
+    const sTerm = searchTerm.val().trim();
+    clearView();
+    searchTermAppend.text("for \"" + sTerm + "\"");
+    if(!sTerm) {
+      showRequiredMsg();
+      return;
+    }
     fetchArticles(
-      searchTerm.val().trim() || "google",
+      sTerm,
       startYear.val().trim() ? startYear.val().trim() + "0101" : "20160101",
       endYear.val().trim() ? endYear.val().trim() + "0101" : "20170228",
       +numberOfRecords.val().trim() || 10);
   });
 
+  $("#clear-btn").on("click", () => {
+    clearView();
+  });
 });
